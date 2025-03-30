6e16abd6b0b0f522b1dca109dabb6a2213f7508d
Dev Bhakat <devrock.pvt@gmail.com>
2025-03-30 18:24:15 +0530

    Update crash-handler.js

--- handlers/crash-handler.js
+++ handlers/crash-handler.js
@@ -1,5 +1,5 @@
 module.exports = (client) => {
-    const targetServerID = '1165841460751507468'; // Define targetServerID directly here
+    const targetServerID = '1301573144817045524'; // Define targetServerID directly here
 
     // Handle uncaught exceptions
     process.on('uncaughtException', (error) => {
